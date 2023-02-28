f0f1f8539011ce81b71aed8a44a86b7c106fec2c
Diffeomorphisme <nicosfaction@msn.com>
2023-02-28 10:01:09 +0100

    finishing day14

--- app/days/day14/day14.py
+++ app/days/day14/day14.py
@@ -1,12 +1,4 @@
-from dataclasses import dataclass
 from copy import deepcopy
-
-
-@dataclass
-class Point:
-    x: int
-    y: int
-    value: str  # air or wall
 
 
 def read_file():
@@ -26,20 +18,28 @@ def read_file():
                     x_plus_1 = coordinates[index + 1][0]
                     y_plus_1 = coordinates[index + 1][1]
                     if x == x_plus_1:
-
-                        walls.extend([[x, _] for _ in range(y, y_plus_1 + 1)])
+                        walls.extend([[x, _]
+                                      for _ in range(min(y, y_plus_1),
+                                                     max(y, y_plus_1) + 1)])
                     elif y == y_plus_1:
-                        walls.extend([[_, y] for _ in range(x_plus_1, x + 1)])
+                        walls.extend([[_, y]
+                                      for _ in range(min(x, x_plus_1),
+                                                     max(x, x_plus_1) + 1)])
     unique_walls = set(tuple(wall) for wall in walls)
-    return list(unique_walls)
+    return tuple(unique_walls)
 
 
-def get_next_move(x, y, walls: list[tuple], min_x, min_y, max_x, max_y):
-    move_one_under = (x, y + 1)
+def get_next_move(x, y, walls: tuple[tuple[int]], min_x, min_y, max_x, max_y):
+    walls_under_sand = tuple(wall for wall in walls if (wall[0] == x and
+                                                        wall[1] > y))
+    if not walls_under_sand:
+        return None
+    highest_wall_position = min(tuple(_[1] for _ in walls_under_sand))
+    move_big_under = (x, highest_wall_position - 1)
     move_diagonal_left = (x - 1, y + 1)
     move_diagonal_right = (x + 1, y + 1)
-    if move_one_under not in walls:
-        return move_one_under
+    if move_big_under != (x, y):
+        return move_big_under
     elif move_diagonal_left not in walls:
         return move_diagonal_left
     elif move_diagonal_right not in walls:
@@ -48,12 +48,11 @@ def get_next_move(x, y, walls: list[tuple], min_x, min_y, max_x, max_y):
 
 def first_part():
     walls = read_file()
-    print((503,13) in walls)
     old_walls = 0
-    min_x = min([wall[0] for wall in walls])
-    max_x = max([wall[0] for wall in walls])
+    min_x = min(tuple(wall[0] for wall in walls))
+    max_x = max(tuple(wall[0] for wall in walls))
     min_y = 0
-    max_y = max([wall[1] for wall in walls])
+    max_y = max(tuple(wall[1] for wall in walls))
     count = 0
     while old_walls != walls:
         old_walls = deepcopy(walls)
@@ -65,27 +64,67 @@ def first_part():
                                       max_x, max_y)
             if next_move:
                 (x_sand, y_sand) = next_move
-                for line in range(max_y + 1):
-                    text = ""
-                    for char in range(min_x, max_x + 1):
-                        if (char,line) in walls:
-                            text += "#"
-                        elif (char,line) == (x_sand, y_sand):
-                            text +="o"
-                        else:
-                            text += "."
-                    print(line, text)
-                print("-"*10)
+                # for line in range(max_y + 1):
+                #     text = ""
+                #     for char in range(min_x, max_x + 1):
+                #         if (char, line) in walls:
+                #             text += "#"
+                #         elif (char, line) == (x_sand, y_sand):
+                #             text +="o"
+                #         else:
+                #             text += "."
+                #     print(line, text)
+                # print("-"*10)
 
                 if x_sand < min_x or x_sand > max_x or y_sand > max_y:
                     break
                 continue
-            walls.append((x_sand, y_sand))
+            walls = walls + ((x_sand, y_sand),)
             count += 1
             break
     return count
 
 
-
 def second_part():
-    pass
+    walls = read_file()
+    old_walls = 0
+    min_y = 0
+    max_y = max([wall[1] for wall in walls]) + 2
+    min_x = min(min([wall[0] for wall in walls]), 500 - max_y)
+    max_x = max(max([wall[0] for wall in walls]), 500 + max_y)
+    walls = walls + tuple((_, max_y)
+                          for _ in range(min_x, max_x + 1))
+    count = 0
+    while old_walls != walls:
+        print(count)
+        old_walls = deepcopy(walls)
+        x_sand = 500
+        y_sand = 0
+        while True:
+            next_move = get_next_move(x_sand, y_sand, walls,
+                                      min_x, min_y,
+                                      max_x, max_y)
+            if next_move:
+                (x_sand, y_sand) = next_move
+                # for line in range(max_y + 1):
+                #     text = ""
+                #     for char in range(min_x, max_x + 1):
+                #         if (char, line) in walls:
+                #             text += "#"
+                #         elif (char, line) == (x_sand, y_sand):
+                #             text +="o"
+                #         else:
+                #             text += "."
+                #     print(line, text)
+                # print("-"*10)
+
+                if x_sand < min_x or x_sand > max_x or y_sand > max_y:
+                    break
+                continue
+            if x_sand == 500 and y_sand == 0:
+                count += 1
+                break
+            walls = walls + ((x_sand, y_sand),)
+            count += 1
+            break
+    return count
